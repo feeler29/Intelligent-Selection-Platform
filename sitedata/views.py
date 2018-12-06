04255ee2e2779795af77b124a819679998a39fa4
feeler29 <feeler29@gmail.com>
2018-12-06 10:09:14 +0800

    Update views.py

--- sitedata/views.py
+++ sitedata/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from django.views.generic.edit import CreatView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from django.urls import reverse_lazy
 
