04d42b1648460709972aaa8cb422d184aba72875
feeler29 <feeler29@gmail.com>
2018-12-06 11:13:03 +0800

    Update urls.py

--- cc/urls.py
+++ cc/urls.py
@@ -22,8 +22,8 @@ from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('enginedata/',include('enginedata.urls')),
-    path('sitedata/',include('sitedata.urls')),
+    path('',include('enginedata.urls')),
+    path('',include('sitedata.urls')),
     #path(r'^i18n/', include('django.conf.urls.i18n')),
  #   path('i18n/', include('django.conf.urls.i18n')),
 #home page is redirected to enginedata page, change later
