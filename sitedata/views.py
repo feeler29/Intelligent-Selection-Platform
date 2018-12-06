b41cda6375f0b07c240021e2d24cf2ca20baa884
feeler29 <feeler29@gmail.com>
2018-12-06 16:26:28 +0800

    Update views.py

--- sitedata/views.py
+++ sitedata/views.py
@@ -4,7 +4,7 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from django.urls import reverse_lazy
 
-from sitedata.models import SiteData,ElectricUseData,FuelUseData,ThermalLoads
+from sitedata.models import SiteData
 
 class SiteDataCreate(CreateView):
 	model = SiteData
@@ -15,25 +15,25 @@ class SiteDataUpdate(UpdateView):
 	model = SiteData
 	fields = '__all__'
 
-class ElectricDataCreate(CreateView):
-	model = ElectricUseData
-	fields = '__all__'
-	success_url = reverse_lazy('fuel_data_create')
+# class ElectricDataCreate(CreateView):
+# 	model = ElectricUseData
+# 	fields = '__all__'
+# 	success_url = reverse_lazy('fuel_data_create')
 
-class FuelDataCreate(CreateView):
-	model = FuelUseData
-	fields = '__all__'
-	success_url = reverse_lazy('thermal_data_create')
+# class FuelDataCreate(CreateView):
+# 	model = FuelUseData
+# 	fields = '__all__'
+# 	success_url = reverse_lazy('thermal_data_create')
 
-class ThermalDataCreate(CreateView):
-	model = ThermalLoads
-	fields = '__all__'
-	success_url = reverse_lazy('thermal_data_update')
+# class ThermalDataCreate(CreateView):
+# 	model = ThermalLoads
+# 	fields = '__all__'
+# 	success_url = reverse_lazy('thermal_data_update')
 
-class ThermalDataUpdate(UpdateView):
-	model = ThermalLoads
-	fields = '__all__'
-	#success_url = reverse_lazy('result')
+# class ThermalDataUpdate(UpdateView):
+# 	model = ThermalLoads
+# 	fields = '__all__'
+# 	#success_url = reverse_lazy('result')
 
 
 
