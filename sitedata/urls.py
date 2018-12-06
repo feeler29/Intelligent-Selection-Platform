737ffe69becc486d6de5f93e8abbb10d20e86504
feeler29 <feeler29@gmail.com>
2018-12-06 10:31:52 +0800

    update form

--- sitedata/urls.py
+++ sitedata/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('sitedata/',views.SiteDataCreate.as_view(), name='site_data_create'),
-    path('sitedata/<int:pk>/update',views.SiteDataUpdate.as_view, name='site_data_update'),
+    path('sitedata/<int:pk>/update',views.SiteDataUpdate.as_view(), name='site_data_update'),
     path('electricdata/create',views.ElectricDataCreate.as_view(),name='electric_data_create'),
     path('fueldata/create',views.FuelDataCreate.as_view(),name='fuel_data_create'),
     path('thermaldata/create',views.ThermalDataCreate.as_view(),name='thermal_data_create'),
