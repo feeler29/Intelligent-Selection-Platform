d6830d84ccf0e7d1087ce23376b98d048cff5718
feeler29 <feeler29@gmail.com>
2018-12-06 10:06:40 +0800

    update

--- sitedata/urls.py
+++ sitedata/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('sitedata/creat',views.SiteDataCreat.as_view(), name='site_data_creat'),
+    path('sitedata/',views.SiteDataCreat.as_view(), name='site_data_creat'),
     path('sitedata/<int:pk>/update',views.SiteDataUpdate.as_view, name='site_data_update'),
     path('electricdata/creat',views.ElectricDataCreat.as_view(),name='electric_data_creat'),
     path('fueldata/creat',views.FuelDataCreat.as_view(),name='fuel_data_creat'),
