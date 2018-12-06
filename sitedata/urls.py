be500d4add1960e756b86671e15d14bbeacef735
feeler29 <feeler29@gmail.com>
2018-12-06 10:12:00 +0800

    Update urls.py

--- sitedata/urls.py
+++ sitedata/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('sitedata/',views.SiteDataCreat.as_view(), name='site_data_create'),
+    path('sitedata/',views.SiteDataCreate.as_view(), name='site_data_create'),
     path('sitedata/<int:pk>/update',views.SiteDataUpdate.as_view, name='site_data_update'),
-    path('electricdata/create',views.ElectricDataCreat.as_view(),name='electric_data_create'),
-    path('fueldata/create',views.FuelDataCreat.as_view(),name='fuel_data_create'),
-    path('thermaldata/create',views.ThermalDataCreat.as_view(),name='thermal_data_create'),
+    path('electricdata/create',views.ElectricDataCreate.as_view(),name='electric_data_create'),
+    path('fueldata/create',views.FuelDataCreate.as_view(),name='fuel_data_create'),
+    path('thermaldata/create',views.ThermalDataCreate.as_view(),name='thermal_data_create'),
 ]
