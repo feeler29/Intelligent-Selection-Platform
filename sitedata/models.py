17fb632fcfb4403bcdbf217f61e99f8d04ace162
feeler29 <feeler29@gmail.com>
2018-12-07 10:22:20 +0800

    Update models.py

--- sitedata/models.py
+++ sitedata/models.py
@@ -15,30 +15,36 @@ class SiteData(models.Model):
 		)
 
 	operating_hours = models.IntegerField(
-		verbose_name=_('Operating hours per day')
+		verbose_name=_('Operating hours per day'),
+		validators=[MaxValueValidator(24),MinValueValidator(0)]
 		)
 
 	operating_days = models.IntegerField(
-		verbose_name=_('Operating days per week')
+		verbose_name=_('Operating days per week'),
+		validators=[MaxValueValidator(7),MinValueValidator(0)]
 		)
 
 	operating_weeks = models.IntegerField(
-		verbose_name=_('Operating weeks per year')
+		verbose_name=_('Operating weeks per year'),
+		validators=[MaxValueValidator(54),MinValueValidator(0)]
 		)
 
 	alitude = models.IntegerField(
 		verbose_name=_('Alitude (m)'),
 		default=900,
+		validators=[MaxValueValidator(5000),MinValueValidator(0)]
 		)
 
 	summer_temp = models.FloatField(
 		verbose_name=_('Summer design temperature'),
 		default=30,
+		validators=[MaxValueValidator(50),MinValueValidator(0)]
 		)
 
 	winter_temp = models.FloatField(
 		verbose_name=_('Winter design temperature'),
 		default=-10,
+		validators=[MaxValueValidator(50),MinValueValidator(-50)]
 		)
 
 
@@ -49,19 +55,78 @@ class SiteData(models.Model):
 		)
 
 	service_voltage = models.FloatField(
-		verbose_name=_('Service voltage (kV)')
+		verbose_name=_('Service voltage (kV)'),
+		validators=[MaxValueValidator(40),MinValueValidator(0)]
 		)
 
 	ave_elec_demand_Jan = models.FloatField(
-		verbose_name=_('Average electric demand January (kW)')
+		verbose_name=_('Average electric demand January (kW)'),
+		validators=[MinValueValidator(0)]
 		)
 
-	peak_dlec_demand_Jan = models.FloatField(
-		verbose_name=_('Peak electric demand January (kW)')
+	ave_elec_demand_Feb = models.FloatField(
+		verbose_name=_('Average electric demand February (kW)'),
+		validators=[MinValueValidator(0)]
+		)
+
+	ave_elec_demand_Mar = models.FloatField(
+		verbose_name=_('Average electric demand March (kW)'),
+		validators=[MinValueValidator(0)]
+		)
+
+	ave_elec_demand_Apr = models.FloatField(
+		verbose_name=_('Average electric demand April (kW)'),
+		validators=[MinValueValidator(0)]
+		)
+
+	ave_elec_demand_May = models.FloatField(
+		verbose_name=_('Average electric demand May (kW)'),
+		validators=[MinValueValidator(0)]
+		)
+
+	ave_elec_demand_Jun = models.FloatField(
+		verbose_name=_('Average electric demand June (kW)'),
+		validators=[MinValueValidator(0)]
+		)
+
+	ave_elec_demand_Jul = models.FloatField(
+		verbose_name=_('Average electric demand July (kW)'),
+		validators=[MinValueValidator(0)]
+		)
+
+	ave_elec_demand_Aug = models.FloatField(
+		verbose_name=_('Average electric demand August (kW)'),
+		validators=[MinValueValidator(0)]
+		)
+
+	ave_elec_demand_Sep = models.FloatField(
+		verbose_name=_('Average electric demand September (kW)'),
+		validators=[MinValueValidator(0)]
+		)
+
+	ave_elec_demand_Otc = models.FloatField(
+		verbose_name=_('Average electric demand October (kW)'),
+		validators=[MinValueValidator(0)]
+		)
+
+	ave_elec_demand_Nov = models.FloatField(
+		verbose_name=_('Average electric demand November (kW)'),
+		validators=[MinValueValidator(0)]
+		)
+
+	ave_elec_demand_Dec = models.FloatField(
+		verbose_name=_('Average electric demand December (kW)'),
+		validators=[MinValueValidator(0)]
+		)
+
+	peak_elec_demand_Jan = models.FloatField(
+		verbose_name=_('Peak electric demand January (kW)'),
+		validators=[MinValueValidator(0)]
 		)
 
 	elec_consum = models.FloatField(
-	    verbose_name=_('Electric consumption (kWh)')
+	    verbose_name=_('Electric consumption (kWh)'),
+	    validators=[MinValueValidator(0)]
 	    )
 
 
@@ -96,20 +161,23 @@ class SiteData(models.Model):
 		)
 
 	steamdemand_max = models.FloatField(
-		verbose_name=_('Maximum Steam Demand (t/h)')
-
+		verbose_name=_('Maximum Steam Demand (t/h)'),
+		validators=[MinValueValidator(0)]
 		)
 
 	steamdemand_ave = models.FloatField(
-		verbose_name=_('Average Steam Demand')
+		verbose_name=_('Average Steam Demand'),
+		validators=[MinValueValidator(0)]
 		)
 
 	steam_temp = models.FloatField(
-		verbose_name=_('Required Steam Temperature')
+		verbose_name=_('Required Steam Temperature'),
+		validators=[MinValueValidator(0)]
 		)
 
 	steam_press = models.FloatField(
-		verbose_name=_('Required Steam Pressure')
+		verbose_name=_('Required Steam Pressure'),
+		validators=[MinValueValidator(0)]
 		)
 
 	track = models.BooleanField(
@@ -123,4 +191,7 @@ class SiteData(models.Model):
 		return reverse('result',kwargs={'pk':self.pk})
 
 class Result(models.Model):
+	
+	
+
 	pass
